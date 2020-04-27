a85185ede00dc7209f97bd18bb3dcfc6679a67c6
Kayla H <55960449+kaylahendrickson@users.noreply.github.com>
2020-04-27 10:59:50 -0400

    Fixed tab interactions

--- js/index.js
+++ js/index.js
@@ -48,43 +48,43 @@ $ (".tabButton").on("click", function() {
     // $(this).toggleClass("checked");
 
     if ($(this).hasClass("tabButton1")){
-        $("#tab1").removeClass("inactive");
+        // $("#tab1").removeClass("inactive");
         $("#tab1").addClass("active");
-        $(this).find("button").toggleClass("checked");
+        $(this).find("button").addClass("checked");
 
         $("#tab2").removeClass("active");
-        $("#tab2").addClass("inactive");
-        $(".tabButton2").removeClass("checked");
+        // $("#tab2").addClass("inactive");
+        $(".tabButton2").find("button").removeClass("checked");
 
         $("#tab3").removeClass("active");
-        $("#tab3").addClass("inactive");
-        $(".tabButton3").removeClass("checked");
+        // $("#tab3").addClass("inactive");
+        $(".tabButton3").find("button").removeClass("checked");
 
     }
     else if ($(this).hasClass("tabButton2")){
-        $("#tab2").removeClass("inactive");
+        // $("#tab2").removeClass("inactive");
         $("#tab2").addClass("active");
         $(this).find("button").addClass("checked");
 
         $("#tab1").removeClass("active");
-        $("#tab1").addClass("inactive");
-        $(".tabButton1").removeClass("checked");
+        // $("#tab1").addClass("inactive");
+        $(".tabButton1").find("button").removeClass("checked");
 
         $("#tab3").removeClass("active");
-        $("#tab3").addClass("inactive");
-        $(".tabButton3").removeClass("checked");
+        // $("#tab3").addClass("inactive");
+        $(".tabButton3").find("button").removeClass("checked");
     }
     else if ($(this).hasClass("tabButton3")){
-        $("#tab3").removeClass("inactive");
+        // $("#tab3").removeClass("inactive");
         $("#tab3").addClass("active");
         $(this).find("button").addClass("checked");
 
         $("#tab1").removeClass("active");
-        $("#tab1").addClass("inactive");
-        $(".tabButton1").removeClass("checked");
+        // $("#tab1").addClass("inactive");
+        $(".tabButton1").find("button").removeClass("checked");
 
         $("#tab2").removeClass("active");
-        $("#tab2").addClass("inactive");
-        $(".tabButton2").removeClass("checked");
+        // $("#tab2").addClass("inactive");
+        $(".tabButton2").find("button").removeClass("checked");
     };
 });
